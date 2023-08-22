0cdeba29e4159bc3cb1c2e45059937e0233a0740
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2023-08-22 14:39:52 +0200

    Add main function

--- scanwatcher.py
+++ scanwatcher.py
@@ -1,14 +1,18 @@
 import tkinter as tk
 from interface import PhotoInfoApp
 
-# Initialize the Tkinter window
-root = tk.Tk()
-app = PhotoInfoApp(root)
-app.withdraw_window()
 
-app.create_observer()
-print(f"Watching directory: {app.watched_directory}")
+def main():
+    root = tk.Tk()
+    app = PhotoInfoApp(root)
+    app.withdraw_window()
 
-# Run the Tkinter main loop
-root.mainloop()
+    app.create_observer()
+    print(f"Watching directory: {app.watched_directory}")
 
+    # Run the Tkinter main loop
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
