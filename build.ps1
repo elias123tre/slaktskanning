c3e9828fc15e284751d1cd7e2ecc9c21b027980c
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2023-10-23 23:50:55 +0200

    Tweak build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,5 @@
 pyinstaller main.py --onefile --windowed --add-data "icon.png;." --add-data "icon.ico;." --icon "icon.ico"
-rm "dist/Skanning metadata.exe"
-mv "dist/main.exe" "dist/Skanning-metadata.exe"
+if (Test-Path "dist/Skanning-metadata.exe") {
+    Remove-Item "dist/Skanning-metadata.exe"
+}
+Move-Item "dist/main.exe" "dist/Skanning-metadata.exe"
