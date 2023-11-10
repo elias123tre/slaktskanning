b284e6651ed3880304e802479e5d73dc63939ad4
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2023-11-10 23:40:40 +0100

    Fix build script to use correct filename

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
-pyinstaller main.py --onefile --windowed --add-data "icon.png;." --add-data "icon.ico;." --icon "icon.ico"
+pyinstaller window.py --onefile --windowed --add-data "icon.png;." --add-data "icon.ico;." --icon "icon.ico"
 if (Test-Path "dist/Skanning-metadata.exe") {
     Remove-Item "dist/Skanning-metadata.exe"
 }
-Move-Item "dist/main.exe" "dist/Skanning-metadata.exe"
+Move-Item "dist/window.exe" "dist/Skanning-metadata.exe"
